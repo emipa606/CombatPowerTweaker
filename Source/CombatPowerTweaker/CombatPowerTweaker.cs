4fc2f119bbf054232039cbb4fc25eb4c43a81e36
emipa606 <emil.offermo@litet.net>
2021-04-16 13:48:16 +0200

    Should show all pawns in the options now

--- Source/CombatPowerTweaker/CombatPowerTweaker.cs
+++ Source/CombatPowerTweaker/CombatPowerTweaker.cs
@@ -1,4 +1,6 @@
-﻿using Verse;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
 
 namespace CombatPowerTweaker
 {
@@ -7,26 +9,53 @@ namespace CombatPowerTweaker
     {
         static CombatPowerTweaker()
         {
+            SettingsInit();
             SetCombatPowerValues();
         }
 
         public static void SetCombatPowerValues()
         {
             var changedDefs = 0;
+
             foreach (var pawnKind in DefDatabase<PawnKindDef>.AllDefsListForReading)
             {
                 changedDefs++;
-                if (!CombatPowerTweakerMod.settings.modifiedStats.ContainsKey(pawnKind.defName) || CombatPowerTweakerMod.settings.modifiedStats[pawnKind.defName] == CombatPowerTweakerMod.settings.vanillaMemory[pawnKind.defName])
+                if (!CombatPowerTweakerMod.Settings.modifiedStats.ContainsKey(pawnKind.defName) || CombatPowerTweakerMod.Settings.modifiedStats[pawnKind.defName] == CombatPowerTweakerMod.Settings.vanillaMemory[pawnKind.defName])
                 {
                     continue;
                 }
-                pawnKind.combatPower = CombatPowerTweakerMod.settings.modifiedStats[pawnKind.defName];
+
+                pawnKind.combatPower = CombatPowerTweakerMod.Settings.modifiedStats[pawnKind.defName];
             }
+
             if (changedDefs > 0)
             {
                 Log.Message($"CombatPowerTweaker: Changed the combat power for {changedDefs} PawnKinds");
             }
         }
-    }
 
+        private static void SettingsInit()
+        {
+            var thingsWithCombatPower = from PawnKindDef pawnKindDef in DefDatabase<PawnKindDef>.AllDefsListForReading orderby pawnKindDef.label select pawnKindDef;
+
+            if (CombatPowerTweakerMod.Settings.modifiedStats == null)
+            {
+                CombatPowerTweakerMod.Settings.modifiedStats = new Dictionary<string, float>();
+            }
+
+            CombatPowerTweakerMod.Settings.vanillaMemory = new Dictionary<string, float>();
+            CombatPowerTweakerMod.Settings.pawnKindNames = new Dictionary<string, string>();
+            foreach (var pawnKind in thingsWithCombatPower)
+            {
+                CombatPowerTweakerMod.Settings.vanillaMemory[pawnKind.defName] = pawnKind.combatPower;
+                CombatPowerTweakerMod.Settings.pawnKindNames[pawnKind.defName] = pawnKind.label;
+                if (CombatPowerTweakerMod.Settings.modifiedStats.ContainsKey(pawnKind.defName))
+                {
+                    continue;
+                }
+
+                CombatPowerTweakerMod.Settings.modifiedStats[pawnKind.defName] = pawnKind.combatPower;
+            }
+        }
+    }
 }
--- Source/CombatPowerTweaker/CombatPowerTweakerSettings.cs
+++ Source/CombatPowerTweaker/CombatPowerTweakerSettings.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using SettingsHelper;
 using UnityEngine;
@@ -7,19 +6,20 @@ using Verse;
 
 namespace CombatPowerTweaker
 {
-    class CombatPowerTweakerSettings : ModSettings
+    internal class CombatPowerTweakerSettings : ModSettings
     {
-        public Dictionary<string, float> vanillaMemory = new Dictionary<string, float>();
-        public Dictionary<string, string> pawnKindNames = new Dictionary<string, string>();
+        private static readonly float maxValue = 2000f;
+
+        private static Vector2 scrollPosition = Vector2.zero;
+
         public Dictionary<string, float> modifiedStats = new Dictionary<string, float>();
 
-        public override void ExposeData()
-        {
-            base.ExposeData();
-            Scribe_Collections.Look(ref modifiedStats, "modifiedStats", LookMode.Value, LookMode.Value, ref modifiedStatsKeys, ref modifiedStatsValues);
-        }
+        public Dictionary<string, string> pawnKindNames = new Dictionary<string, string>();
+
+        public Dictionary<string, float> vanillaMemory = new Dictionary<string, float>();
 
         private List<string> modifiedStatsKeys;
+
         private List<float> modifiedStatsValues;
 
         public void DoSettingsWindowContents(Rect inRect)
@@ -27,49 +27,55 @@ namespace CombatPowerTweaker
             var keys = modifiedStats.Keys.ToList();
             keys.Reverse();
             var rect = new Rect(inRect.x, inRect.y, inRect.width, inRect.height);
-            var rect2 = new Rect(0f, 0f, inRect.width - 30f, keys.Count * 25);
-            Widgets.BeginScrollView(rect, ref scrollPosition, rect2, true);
+            var rect2 = new Rect(0f, 0f, inRect.width - 30f, keys.Count * 36);
+            Widgets.BeginScrollView(rect, ref scrollPosition, rect2);
             var listingStandard = new Listing_Standard();
             listingStandard.Begin(rect2);
             listingStandard.Label("Combat power, vanilla value in parenthesis");
             if (listingStandard.ButtonTextLabeled("Reset all to vanilla", "Reset"))
             {
-                foreach (var keyValue in CombatPowerTweakerMod.settings.vanillaMemory)
+                foreach (var keyValue in CombatPowerTweakerMod.Settings.vanillaMemory)
                 {
-                    CombatPowerTweakerMod.settings.modifiedStats[keyValue.Key] = keyValue.Value;
+                    CombatPowerTweakerMod.Settings.modifiedStats[keyValue.Key] = keyValue.Value;
                 }
             }
+
             if (listingStandard.ButtonTextLabeled("Increase all values by 10%", "+10%"))
             {
-                foreach (var keyValue in CombatPowerTweakerMod.settings.vanillaMemory)
+                foreach (var keyValue in CombatPowerTweakerMod.Settings.vanillaMemory)
                 {
-                    CombatPowerTweakerMod.settings.modifiedStats[keyValue.Key] = CombatPowerTweakerMod.settings.modifiedStats[keyValue.Key] * 1.1f;
-                    if (CombatPowerTweakerMod.settings.modifiedStats[keyValue.Key] > maxValue)
+                    CombatPowerTweakerMod.Settings.modifiedStats[keyValue.Key] = CombatPowerTweakerMod.Settings.modifiedStats[keyValue.Key] * 1.1f;
+                    if (CombatPowerTweakerMod.Settings.modifiedStats[keyValue.Key] > maxValue)
                     {
-                        CombatPowerTweakerMod.settings.modifiedStats[keyValue.Key] = maxValue;
+                        CombatPowerTweakerMod.Settings.modifiedStats[keyValue.Key] = maxValue;
                     }
                 }
             }
+
             if (listingStandard.ButtonTextLabeled("Decrease all values by 10%", "-10%"))
             {
-                foreach (var keyValue in CombatPowerTweakerMod.settings.vanillaMemory)
+                foreach (var keyValue in CombatPowerTweakerMod.Settings.vanillaMemory)
                 {
-                    CombatPowerTweakerMod.settings.modifiedStats[keyValue.Key] = CombatPowerTweakerMod.settings.modifiedStats[keyValue.Key] * 0.9f;
+                    CombatPowerTweakerMod.Settings.modifiedStats[keyValue.Key] = CombatPowerTweakerMod.Settings.modifiedStats[keyValue.Key] * 0.9f;
                 }
             }
+
             for (var num = keys.Count - 1; num >= 0; num--)
             {
                 var test = modifiedStats[keys[num]];
-                listingStandard.AddLabeledSlider($"{GenText.CapitalizeFirst(pawnKindNames[keys[num]])} ({vanillaMemory[keys[num]]})", ref test, 1f, maxValue, test.ToString(), null, 1);
+                listingStandard.AddLabeledSlider($"{pawnKindNames[keys[num]].CapitalizeFirst()} ({vanillaMemory[keys[num]]})", ref test, 1f, maxValue, test.ToString(), null, 1);
                 modifiedStats[keys[num]] = test;
             }
+
             listingStandard.End();
             Widgets.EndScrollView();
             Write();
         }
 
-        private static Vector2 scrollPosition = Vector2.zero;
-
-        private static readonly float maxValue = 2000f;
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(ref modifiedStats, "modifiedStats", LookMode.Value, LookMode.Value, ref modifiedStatsKeys, ref modifiedStatsValues);
+        }
     }
 }
